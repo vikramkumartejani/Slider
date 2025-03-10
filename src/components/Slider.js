28878d20162ec0b0699d46a698c7597efc9861d0
vikram kumar <vikramkumar8t8@gmail.com>
2025-03-10 08:31:58 +0500

    update

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -8,7 +8,7 @@ import ThirdSliderImage from "../assets/third-slider-image.jpg";
 import FourthSliderImage from "../assets/fourth-slider-image.jpg";
 import FifthSliderImage from "../assets/fifth-slider-image.jpg";
 import Thumb from "../assets/thumb.svg";
-import Thumb2 from "../assets/thumb2.png";
+import Thumb2 from "../assets/thumb2.svg";
 import Thumb3 from "../assets/thumb3.svg";
 import Thumb4 from "../assets/thumb4.svg";
 import Thumb5 from "../assets/thumb5.svg";
@@ -39,7 +39,7 @@ const Slider = () => {
       <motion.div
         className="md:h-screen h-[440px] overflow-hidden relative rounded-2xl md:rounded-none"
       >
-        <AnimatePresence mode="wait">
+        <AnimatePresence maintainExitVelocity={false}>
           <motion.img
             key={currentSlide}
             src={slides[currentSlide].image}
@@ -48,6 +48,9 @@ const Slider = () => {
             style={{
               objectFit: "cover",
             }}
+            animate={{ opacity: 1, scale: 1 }}
+            exit={{ opacity: 0.5, scale:1 }}
+            transition={{ duration: 0.8, ease: "easeInOut" }}
           />
         </AnimatePresence>
 
@@ -67,13 +70,13 @@ const Slider = () => {
         {/* Navigation container - responsive positioning */}
         <div
           className={`absolute h-full md:h-screen z-20 ${isMobile
-            ? "bottom-[4%] left-0 w-full flex justify-center items-center"
+            ? "bottom-0 md:bottom-[4%] left-0 w-full flex justify-center items-center"
             : "top-0 right-0 w-full flex justify-end items-center"
             }`}
         >
           <div
             className={`relative h-full md:h-screen ${isMobile
-              ? "h-[120px] w-full max-w-[90vw] px-4"
+              ? "h-[120px] w-full max-w-[80%]"
               : "h-[180px] w-full max-w-[400px]"
               } flex items-center justify-center`}
           >
@@ -81,7 +84,7 @@ const Slider = () => {
               let x, y, angle, radians;
 
               if (isMobile) {
-                const spacing = 65;
+                const spacing = 70;
                 const centerOffset = (currentSlide - index) * spacing;
                 x = -centerOffset;
                 y = Math.abs(centerOffset) * 0.5;
@@ -111,20 +114,21 @@ const Slider = () => {
                   key={index}
                   onClick={() => setCurrentSlide(index)}
                   className="absolute"
+                  // initial={{ opacity: 0 }}
                   animate={{
                     opacity: 1,
                     x: x,
                     y: y,
-                    top: isMobile ? "90%" : "50%",
+                    top: isMobile ? "85%" : "50%",
                     right: isMobile ? "auto" : "60%",
                     marginTop: -size / 2,
                     marginRight: isMobile ? 0 : -size / 2,
                   }}
                   transition={{
-                    duration: 0.5,
+                    duration: 0.3,
                     type: "spring",
-                    stiffness: 300,
-                    damping: 30,
+                    stiffness: 200,
+                    damping: 80,
                   }}
                   style={{
                     zIndex: zIndex,
@@ -142,7 +146,7 @@ const Slider = () => {
                     <motion.img
                       src={slide.thumb}
                       alt={`nav ${index + 1}`}
-                      className="object-cover"
+                      className="object-cover rotate-90"
                       animate={{
                         width: size - 6,
                         height: size - 6,
